70ce829bf2c07c80e20975eed002a191a4984b14
dori medini <dorimedini@gmail.com>
2015-06-09 20:04:21 +0300

    סקריפטים להתקנה/הסרה של ההתקן וסקריפט לעדכון קבצים

---
+++ scripts/uninstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Go to the correct directory
+cd /root/hw4
+
+# Do some cleanup
+rm -f /dev/snake*
+rmmod snake
+
---
+++ scripts/update_files.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Go home
+cd /root/hw4
+
+# Copy
+cp -vr /mnt/hgfs/HW/234123-hw4/our_files/* ./
+cp -vr /mnt/hgfs/HW/234123-hw4/course_files/snake.h ./
+cp -vr /mnt/hgfs/HW/234123-hw4/scripts/*install.sh ./
+
+# Unixify
+dos2unix ./*
+
+# Allow script usage
+chmod a+x ./*install.sh
+
